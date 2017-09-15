ec34a91a0f8096873a551f60e41a387db6f22f81
Dan Leehr <dan.leehr@duke.edu>
2017-09-15 09:46:30 -0400

    Update required DukeDSClient to 0.3.18
    This version includes retries for swift 401 unauthorized among other fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 LANDO_REQUIREMENTS = [
       "shade==1.23.0",
       "cwlref-runner==1.0",
-      "DukeDSClient==0.3.17",
+      "DukeDSClient==0.3.18",
       "humanfriendly==2.4",
       "Jinja2==2.9.5",
       "lando-messaging==0.7.2",
