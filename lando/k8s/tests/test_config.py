ff318315b72bf4c40375751a315798ea980cbae7
John Bradley <johnbradley2008@gmail.com>
2019-02-28 11:22:21 -0500

    update README and remove unused config options
    Removes settings no longer applicable in config.

--- lando/k8s/tests/test_config.py
+++ lando/k8s/tests/test_config.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
-from lando.k8s.config import create_server_config, InvalidConfigException, ServerConfig, DEFAULT_REQUESTED_MEMORY, \
-    DEFAULT_REQUESTED_CPU
+from lando.k8s.config import create_server_config, InvalidConfigException, ServerConfig
 from unittest.mock import patch
 import logging
 
@@ -25,21 +24,8 @@ MINIMAL_CONFIG = {
     'data_store_settings': {
         'secret_name': 'ddsclient-secret'
     },
-    'stage_data_settings': {
-        'image_name': 'lando-util:1',
-        'command': 'download.py',
-    },
     'run_workflow_settings': {},
-    'organize_output_settings': {
-        'image_name': 'lando-util:2',
-        'command': 'organize.py',
-    },
-    'save_output_settings': {
-        'image_name': 'lando-util:3',
-        'command': 'upload.py',
-    },
     'record_output_project_settings': {
-        'image_name': 'lachlanevenson/k8s-kubectl',
         'service_account_name': 'annotation-writer-sa',
     },
 }
@@ -67,34 +53,13 @@ FULL_CONFIG = {
     'data_store_settings': {
         'secret_name': 'ddsclient-secret'
     },
-    'stage_data_settings': {
-        'image_name': 'lando-util:1',
-        'command': 'download.py',
-        'requested_cpu': 2,
-        'requested_memory': '2G',
-    },
     'run_workflow_settings': {
-        'requested_cpu': 3,
-        'requested_memory': '3G',
         'system_data_volume': {
             'mount_path': '/system/data',
             'volume_claim_name': 'system-data',
         }
     },
-    'organize_output_settings': {
-        'image_name': 'lando-util:2',
-        'command': 'organize.py',
-        'requested_cpu': 4,
-        'requested_memory': '4G',
-    },
-    'save_output_settings': {
-        'image_name': 'lando-util:3',
-        'command': 'upload.py',
-        'requested_cpu': 5,
-        'requested_memory': '5G',
-    },
     'record_output_project_settings': {
-        'image_name': 'lachlanevenson/k8s-kubectl',
         'service_account_name': 'annotation-writer-sa',
     },
     'storage_class_name': 'gluster'
@@ -126,29 +91,8 @@ class TestServerConfig(TestCase):
         self.assertEqual(config.cluster_api_settings.verify_ssl, True)
 
         self.assertIsNotNone(config.bespin_api_settings)
-
         self.assertEqual(config.data_store_settings.secret_name, 'ddsclient-secret')
-
-        self.assertEqual(config.stage_data_settings.image_name, 'lando-util:1')
-        self.assertEqual(config.stage_data_settings.command, 'download.py')
-        self.assertEqual(config.stage_data_settings.requested_cpu, DEFAULT_REQUESTED_CPU)
-        self.assertEqual(config.stage_data_settings.requested_memory, DEFAULT_REQUESTED_MEMORY)
-
-        self.assertEqual(config.run_workflow_settings.requested_cpu, DEFAULT_REQUESTED_CPU)
-        self.assertEqual(config.run_workflow_settings.requested_memory, DEFAULT_REQUESTED_MEMORY)
         self.assertEqual(config.run_workflow_settings.system_data_volume, None)
-
-        self.assertEqual(config.organize_output_settings.image_name, 'lando-util:2')
-        self.assertEqual(config.organize_output_settings.command, 'organize.py')
-        self.assertEqual(config.organize_output_settings.requested_cpu, DEFAULT_REQUESTED_CPU)
-        self.assertEqual(config.organize_output_settings.requested_memory, DEFAULT_REQUESTED_MEMORY)
-
-        self.assertEqual(config.save_output_settings.image_name, 'lando-util:3')
-        self.assertEqual(config.save_output_settings.command, 'upload.py')
-        self.assertEqual(config.save_output_settings.requested_cpu, DEFAULT_REQUESTED_CPU)
-        self.assertEqual(config.save_output_settings.requested_memory, DEFAULT_REQUESTED_MEMORY)
-
-        self.assertEqual(config.record_output_project_settings.image_name, 'lachlanevenson/k8s-kubectl')
         self.assertEqual(config.record_output_project_settings.service_account_name, 'annotation-writer-sa')
 
         self.assertEqual(config.storage_class_name, None)
@@ -156,14 +100,6 @@ class TestServerConfig(TestCase):
     def test_optional_config(self):
         config = ServerConfig(FULL_CONFIG)
         self.assertEqual(config.log_level, logging.DEBUG)
-        self.assertEqual(config.stage_data_settings.requested_cpu, 2)
-        self.assertEqual(config.stage_data_settings.requested_memory, '2G')
-        self.assertEqual(config.run_workflow_settings.requested_cpu, 3)
-        self.assertEqual(config.run_workflow_settings.requested_memory, '3G')
         self.assertEqual(config.run_workflow_settings.system_data_volume.mount_path, '/system/data')
         self.assertEqual(config.run_workflow_settings.system_data_volume.volume_claim_name, 'system-data')
-        self.assertEqual(config.organize_output_settings.requested_cpu, 4)
-        self.assertEqual(config.organize_output_settings.requested_memory, '4G')
-        self.assertEqual(config.save_output_settings.requested_cpu, 5)
-        self.assertEqual(config.save_output_settings.requested_memory, '5G')
         self.assertEqual(config.cluster_api_settings.verify_ssl, False)
