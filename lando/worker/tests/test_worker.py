9f2501645e48b214ff5a25bfdf301b8af52f5af4
Dan Leehr <dan.leehr@duke.edu>
2019-04-04 13:18:47 -0400

    Update FakeObject.run_workflow to match CwlWorkflow.run()

--- lando/worker/tests/test_worker.py
+++ lando/worker/tests/test_worker.py
@@ -75,7 +75,7 @@ class FakeObject(object):
         self.report.add(self.run_message)
         return Mock(remote_id='2348')
 
-    def run_workflow(self, cwl_file_url, workflow_object_name, job_order):
+    def run_workflow(self, workflow_type, workflow_url, workflow_path, job_order):
         self.report.add(self.run_message)
 
     def job_step_error(self, payload, message):
